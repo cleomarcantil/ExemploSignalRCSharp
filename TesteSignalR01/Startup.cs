1eb98b3e82988c23ffa2cbe09e12d209f54b5fa3
Cleomar Cantil <cleomarcantil@outlook.com>
2021-08-01 17:33:39 -0300

    Atualização do .NET para versão 6.0. Removido arquivos não usados.

--- TesteSignalR01/Startup.cs
+++ TesteSignalR01/Startup.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using TesteSignalR01.Hubs;
 using TesteSignalR01.Services;
 
@@ -14,12 +10,19 @@ namespace TesteSignalR01
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc()
-				.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddControllers();
+
+			services.AddRazorPages();
 
 			services.AddHostedService<ClockHostedService>();
 
@@ -27,28 +30,32 @@ namespace TesteSignalR01
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.UseSignalR(route =>
-			{
-				route.MapHub<ChatHub>("/chathub");
-				route.MapHub<ClockHub>("/clockhub");
-			});
+			app.UseHttpsRedirection();
 
-			//app.Run(async (context) =>
-			//{
-			//	await context.Response.WriteAsync("Hello World!");
-			//});
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapRazorPages();
+
+				endpoints.MapHub<ChatHub>("/chathub");
+				endpoints.MapHub<ClockHub>("/clockhub");
+			});
 
 			app.UseDefaultFiles(new DefaultFilesOptions { DefaultFileNames = new[] { "index.html" } });
 			app.UseStaticFiles();
 
-			app.UseMvc();
 		}
+
 	}
+
 }
